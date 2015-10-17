da0d195c4a361e09400463219d90a5d3c7ae5ace
Artur Lane <arturlan.us@gmail.com>
2015-10-17 18:03:05 -0400

    unit-2-assessment

--- unit-2-assessment/unit-2-assessment/ViewController.m
+++ unit-2-assessment/unit-2-assessment/ViewController.m
@@ -7,12 +7,48 @@
 //
 
 #import "ViewController.h"
+#import "WeatherTableViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *saveButtonTapped;
 
 @end
 
 @implementation ViewController
+- (IBAction)saveButtonTapped:(UIButton *)sender {
+    NSString *saveLatitude = self.lat.text;
+    NSString *saveLongitude = self.lng.text;
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:saveLatitude forKey:@"savedLatitude"];
+    [defaults setObject:saveLongitude forKey:@"savedLongitude"];
+    [defaults synchronize];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+//    
+//    NSString *latCoordinate = self.lat.text;
+//    NSString *lngCoordinate = self.lng.text;
+//    
+//    
+//    [[NSUserDefaults standardUserDefaults] setObject:latCoordinate forKey:@"savedLatitude"];
+//    [[NSUserDefaults standardUserDefaults] setObject:lngCoordinate forKey:@"savedLongitude"];
+//    [[NSUserDefaults standardUserDefaults] synchronize];
+//    
+//    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:YES];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *loadLat =  [defaults objectForKey:@"savedLatitude"];
+    NSString *loadLng =  [defaults objectForKey:@"savedLongitude"];
+
+    [self.lat setText:loadLat];
+    [self.lng setText:loadLng];
+
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
