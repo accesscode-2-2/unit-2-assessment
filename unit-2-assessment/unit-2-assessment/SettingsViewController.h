cadbee316eb103efbad195aa94221063664ee7c1
Max White <maxjwhite22@gmail.com>
2015-10-17 11:37:30 -0400

    nsuser defaults working

---
+++ unit-2-assessment/unit-2-assessment/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  unit-2-assessment
+//
+//  Created by Max White on 10/17/15.
+//  Copyright © 2015 Michael Kavouras. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SettingsViewController : ViewController
+
+@end
